d07ee421f4cf46f8ac72451ecd55888b8061e023
vlach1989 <vlach1989@gmail.com>
2020-07-09 14:07:32 +0200

    Updates

---
+++ precache-manifest.7f8e5c9ecddf88e0a251f199cd45b076.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60066bf57b11dd8d92b628801f283e8",
+    "url": "/app-s14scienceAmazonas20/index.html"
+  },
+  {
+    "revision": "4d831dc052326ef47a34",
+    "url": "/app-s14scienceAmazonas20/static/css/2.fcbb9a24.chunk.css"
+  },
+  {
+    "revision": "9717f0ed5c72337b19cf",
+    "url": "/app-s14scienceAmazonas20/static/css/main.81207284.chunk.css"
+  },
+  {
+    "revision": "4d831dc052326ef47a34",
+    "url": "/app-s14scienceAmazonas20/static/js/2.6660ee3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app-s14scienceAmazonas20/static/js/2.6660ee3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9717f0ed5c72337b19cf",
+    "url": "/app-s14scienceAmazonas20/static/js/main.4d15b695.chunk.js"
+  },
+  {
+    "revision": "0dbb1703c11fe0481e65",
+    "url": "/app-s14scienceAmazonas20/static/js/runtime-main.cc7e829b.js"
+  },
+  {
+    "revision": "06d5a8448f9c8eb18e564b9900df00c9",
+    "url": "/app-s14scienceAmazonas20/static/media/background.06d5a844.jpg"
+  },
+  {
+    "revision": "814a9544aef76e8740bc3e84dc65f9ed",
+    "url": "/app-s14scienceAmazonas20/static/media/logo.814a9544.jpg"
+  }
+]);

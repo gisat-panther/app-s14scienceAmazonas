322e77e28b1179732846dbac4579c3de4f236d84
PavelVlach <vlach1989@gmail.com>
2022-03-01 08:24:59 +0100

    Add link to app

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,16 @@ var path = require('path');
 module.exports = function override(config, env) {
 	// TODO comment when using package from npm
 	config.resolve = {
-		alias: {
-			// "@gisatcz/ptr-charts": "C:/Users/pvlach/DATA/ptr-charts/dist",
+		// alias: {
+		// 	react:
+		// 		'C:/Users/PavelVlach/WebstormProjects/ptr-atoms/node_modules/react',
+		// 	"@gisatcz/ptr-atoms": "C:/Users/PavelVlach/WebstormProjects/ptr-atoms",
 			// "@gisatcz/ptr-maps": "C:/Users/pvlach/DATA/ptr-maps/dist",
 			// "@gisatcz/ptr-core": "C:/Users/pvlach/DATA/ptr-core",
 			// "@gisatcz/ptr-state": "C:/Users/pvlach/DATA/ptr-state",
 			// "@gisatcz/ptr-components": "C:/Users/pvlach/DATA/ptr-components/dist",
 			// "@gisatcz/ptr-deprecated": "C:/Users/pvlach/DATA/ptr-deprecated/dist",
-		}
+		// }
 	};
 
 	return config;
